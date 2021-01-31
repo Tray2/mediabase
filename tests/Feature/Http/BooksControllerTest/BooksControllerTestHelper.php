fd78b527f66857c526f96267c34e07c3eda88eac
Tray2 <tray2@tray2.se>
2021-01-31 16:46:03 +0100

    Add media_type_id to Testhelpers and type testhelper properties

--- tests/Feature/Http/BooksControllerTest/BooksControllerTestHelper.php
+++ tests/Feature/Http/BooksControllerTest/BooksControllerTestHelper.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\BooksControllerTest;
 
+use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 use App\Models\Author;
 use App\Models\Format;
@@ -9,14 +10,14 @@ use App\Models\Genre;
 
 class BooksControllerTestHelper extends TestCase
 {
-    protected $genre = '';
-    protected $format = '';
-    protected $author = '';
+    protected Collection $genre;
+    protected Collection $format;
+    protected Collection $author;
 
     protected function createForeignKeys($quantity = 1)
     {
-        $this->genre = Genre::factory($quantity)->create();
-        $this->format = Format::factory($quantity)->create();
+        $this->genre = Genre::factory($quantity)->create(['media_type_id' => env('BOOKS')]);
+        $this->format = Format::factory($quantity)->create(['media_type_id' => env('BOOKS')]);
         $this->author = Author::factory($quantity)->create();
     }
 }
--- tests/Feature/Http/RecordsControllerTest/RecordsControllerTestHelper.php
+++ tests/Feature/Http/RecordsControllerTest/RecordsControllerTestHelper.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\RecordsControllerTest;
 
+use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 use App\Models\Artist;
 use App\Models\Format;
@@ -9,14 +10,14 @@ use App\Models\Genre;
 
 class RecordsControllerTestHelper extends TestCase
 {
-    protected $genre = '';
-    protected $format = '';
-    protected $artist = '';
+    protected Collection $genre;
+    protected Collection $format;
+    protected Collection $artist;
 
     protected function createForeignKeys($quantity = 1)
     {
-        $this->genre = Genre::factory($quantity)->create();
-        $this->format = Format::factory($quantity)->create();
+        $this->genre = Genre::factory($quantity)->create(['media_type_id' => env('RECORDS')]);
+        $this->format = Format::factory($quantity)->create(['media_type_id' => env('RECORDS')]);
         $this->artist = Artist::factory($quantity)->create();
     }
 }
