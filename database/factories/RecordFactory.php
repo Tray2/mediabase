666c27a17f9c19e2fe745923089238d3a33cc59b
Tray2 <tray2@tray2.se>
2022-10-02 08:05:37 +0200

    Fix broken tests

--- database/factories/RecordFactory.php
+++ database/factories/RecordFactory.php
@@ -2,9 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Country;
 use App\Models\Format;
 use App\Models\Genre;
 use App\Models\Record;
+use App\Models\RecordLabel;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class RecordFactory extends Factory
@@ -18,6 +20,8 @@ class RecordFactory extends Factory
             'released' => $this->faker->year('now'),
             'genre_id' => Genre::factory()->create(),
             'format_id' => Format::factory()->create(),
+            'record_label_id' => RecordLabel::factory()->create(),
+            'country_id' => Country::factory()->create(),
             'barcode' => $this->faker->creditCardNumber(),
             'spine_code' => $this->faker->creditCardNumber(),
         ];
